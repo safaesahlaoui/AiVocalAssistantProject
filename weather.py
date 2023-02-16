9797f94dc88ba9d3616c2d3c652697d98aef5e90
Safae <safaesahlaoui2001@gmail.com>
2023-02-16 15:58:47 +0100

    api call problem fixed , it works now

--- weather.py
+++ weather.py
@@ -1,14 +1,15 @@
 import requests
 from apiKey import *
 
-api_address='https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid'+key2
+api_address='http://api.openweathermap.org/data/2.5/weather?q=Kenitra,MA&APPID='+key2
 json_data=requests.get(api_address).json()
-
+# i made an xsl file contain name of all known cities with their codes from openweatherapi
 def temp():
-    temperature=round(json_data["main"]["temp"]-273,1)
-    return temperature
+     temperature=round(json_data["main"]["temp"]-273,1)
+     return temperature
 def des():
-    description=json_data["weather"][0]["descrption"]
-    return description
-print(temp())
-print(des())
+     description=json_data["weather"][0]["description"]
+     return description
+
+#print(temp())
+#print(des())
